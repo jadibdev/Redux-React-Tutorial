c9f9689e626033b5dd3bf11335a55d749a3d845c
mac <mac@MacBook-Pro-de-mac-2.local>
2020-10-25 10:22:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import {
   Route,
   Redirect,
 } from 'react-router-dom'
+import PostsList  from './features/posts/PostsList'
 
 import { Navbar } from './app/Navbar'
+import { AddPostForm } from './features/posts/AddPostForm'
 
 function App() {
   return (
@@ -18,9 +20,10 @@ function App() {
             exact
             path="/"
             render={() => (
-              <section>
-                <h2>Welcome to the Redux Essentials example app!</h2>
-              </section>
+              <React.Fragment>
+                <AddPostForm />
+                <PostsList />
+              </React.Fragment>
             )}
           />
           <Redirect to="/" />
